94bbc6033b9719cb813f9f89ed3e178b6e9d8a02
hermannwi <89527413+hermannwi@users.noreply.github.com>
2023-06-10 15:22:44 +0200

    Add files via upload
    Instead of creating all the stories before writing them, the program now appends to the csv file for each response as to not risk cost ineffectiveness if there is an error

--- script/chatGPT_stories_gen.py
+++ script/chatGPT_stories_gen.py
@@ -3,57 +3,51 @@ import openai
 import csv
 from datetime import date
 
-
-
-# Sets the key for the openai api
+# Sets the key for the openai api. You need to refer to the path where you saved your key.txt file
 openai.api_key_path = "key.txt"
 
 # Generates a message based on the marker
-def generate_message(marker):
+def generate_message(category):
     vowels = ('a','e','i','o','u','A','E','I','O','U')
     article = ""
-    if marker[0] in vowels:
+    if category[0] in vowels:
         article = "an"
     else:
         article = "a"
-    return f"Write a 50 word plot summary for {article} {marker} children's novel."
+    return f"Write a 50 word plot summary for {article} {category} children's novel."
 
-# Generates stories based on the user's input
+# Generates stories based on the user's input and writes them to a csv file
 def generate_stories():
     messages = [ {"role": "system", "content": ""} ]
-    stories = []
-    # The user has to specify a marker (American, British, etc.)
-    message = generate_message(input("Enter a marker: "))
+    # The user has to specify a category (American, British, etc.)
+    category = input("Enter a marker: ")
+    message = generate_message(category)
     # The user has to specify how many stories to generate
     n = int(input("Enter the number of stories to generate: "))
-    # For loop that prompts chatgpt a specified number of times
+    # For loop that prompts chatgpt a specified number of times and for each iteration writes the prompt, reply, date and model to a csv file
     for i in range(n):
-        messages.append({"role": "user", "content": message},)
-        # Creates a chatgpt model based on the previous prompts and replies
-        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-        # Removes the last prompt from the messages
-        messages = messages[:-1]
-        # A call to the chatgpt model that generates a reply
-        reply = chat.choices[0].message.content
-        # print(f"ChatGPT: {reply}")
-
-        # Appends the prompt, reply, date, and modelname to the stories list
-        unit = (message, reply, date.today(), "gpt-3.5-turbo") 
-        stories.append(unit)
-        
-        
-    return stories
+        with open (f"{category}_stories.csv", "a", newline="") as f:
+            writer = csv.DictWriter(f, delimiter=";", fieldnames=["prompt", "reply", "date", "modelname"])
+            # If the file is empty, it writes the fieldnames
+            if os.stat(f"{category}_stories.csv").st_size == 0:
+                writer.writeheader()
+            messages.append({"role": "user", "content": message},)
+            # Creates a chatgpt model based on the previous prompts and replies
+            chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+            # Removes the last prompt from the messages
+            messages = messages[:-1]
+            # A call to the chatgpt model that generates a reply
+            reply = chat.choices[0].message.content
+            unit = {"prompt": message, "reply": reply, "date": date.today(), "modelname": "gpt-3.5-turbo"}
+            # writes the prompt, reply, date and model to a csv file
+            writer.writerow(unit)
+            
 
 # Generate stories
 stories = generate_stories()
 
 
-# Writes stories to a csv file
-with open (f"{stories[0][0].split()[8]}_stories.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["prompt", "reply", "date" "modelname"])
-    for story in stories:
-        writer.writerow(story)
+
 
 
 
