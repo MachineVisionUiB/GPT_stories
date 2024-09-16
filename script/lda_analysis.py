d28629ee5504984cf5ed0ba6dc4a070ea75397b5
Hermann Wigers <hermann-wigers@hotmail.com>
2024-09-16 11:04:45 +0200

    loads dataset and preprocesses the data

--- script/lda_analysis.py
+++ script/lda_analysis.py
@@ -1,11 +1,38 @@
 import pandas as pd
 import nltk
+import ssl
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from gensim import corpora
 from gensim.models.ldamodel import LdaModel
 
+
+# Bypass SSL certificate verification
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
 # Load dataset
-df = pd.read_csv('plot_summaries.csv')
-summaries = df['summary'].tolist()
+df = pd.read_csv('/Users/hermannwigers/Documents/AI STORIES/GPT_stories/samples/japanese_name_test.csv')
+summaries = df['Story'].tolist()
+
+# Preprocessing
+nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def preprocess(text):
+    tokens = word_tokenize(text.lower())
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word.isalpha() and word not in stop_words]
+    return tokens
+
+processed_summaries = [preprocess(summary) for summary in summaries]
+
+print(processed_summaries)
