dc1b812d9916b6f2a6708bf0865310fe4cdd811f
hermannwi <89527413+hermannwi@users.noreply.github.com>
2024-11-14 10:40:34 +0100

    comments noun_phrases.py

--- script/noun_phrases.py
+++ script/noun_phrases.py
@@ -3,14 +3,27 @@ from textblob import TextBlob
 from collections import Counter
 import os
 
-
-
-
-
 def extract_noun_phrases(file):
+    """
+    Extract and count noun phrases from a specified column in a CSV file
+    and save the counts to a new CSV file.
+
+    This function reads a CSV file, extracts text from a specified column,
+    and uses TextBlob to identify noun phrases. It counts occurrences of each
+    noun phrase and writes the results to a new CSV file in descending order
+    of frequency.
+
+    Args:
+        file (str): The path to the input CSV file containing text data.
+
+    Example:
+        file = '/path/to/input.csv'
+        extract_noun_phrases(file)
+    """
+    
     df = pd.read_csv(file)
-    # Extract stories from the second column
-    stories = df.iloc[:, 4].tolist()  # Adjust if the column index is different. df.iloc[:, x] where x is the column index
+    # Extract stories from the fifth column
+    stories = df.iloc[:, 4].tolist()  # Adjust if the column index is different
 
     # Extract noun phrases
     noun_phrases = []
@@ -26,11 +39,15 @@ def extract_noun_phrases(file):
     output_df = pd.DataFrame(sorted_noun_phrases, columns=['Noun Phrase', 'Count'])
 
     # Save to a new CSV file
-    output_file = 'noun_phrases_' +  os.path.basename(os.path.normpath(file))  # Replace with your desired output file path
+    output_file = 'noun_phrases_' + os.path.basename(os.path.normpath(file))  # Replace with your desired output file path
     output_df.to_csv(output_file, index=False)
 
-
 if __name__ == "__main__":
+    """
+    Process all CSV files in the specified directory to extract noun phrases,
+    count their occurrences, and save each result to a new CSV file.
+    """
+    
     directory = '/Users/hermannwigers/Documents/AI STORIES/GPT_stories/data/childrens_stories/summaries'
     for filename in os.listdir(directory):
         print(filename)
